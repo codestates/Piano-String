ee437a65257aa95e14f89c76c257182f4f06b44d
Shinho Ahn <fracsinus@gmail.com>
2021-12-24 06:35:54 +0900

    client: Add MusicPlayer to ArticleView page

--- client/src/components/MusicPlayer.js
+++ client/src/components/MusicPlayer.js
@@ -1,9 +1,30 @@
+const { useEffect, useState } = require('react');
+
 export default function MusicPlayer({ music }) {
-  const player = new mm.SoundFontPlayer('https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus');
-  player.callbaclObject = { run: () => {}, stop: () => console.log('done') };
+  const [player, _] = useState(new mm.SoundFontPlayer('https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus'));
+  const [isPlaying, setIsPlaying] = useState(false);
+  player.callbackObject = { run: () => {}, stop: () => { setIsPlaying(false) } };
+
+  const controlPlayer = () => {
+    switch (player.getPlayState()) {
+      case 'stopped':
+        player.start(music);
+        break;
+      case 'started':
+        player.pause();
+        break;
+      case 'paused':
+        player.resume();
+        break;
+    }
+  }
+
   return (
     <div>
-      <button type="button" onClick={() => { player.start(music); }}>Play</button>
+      { isPlaying
+        ? <button type="button" onClick={() => { setIsPlaying(prev => false); controlPlayer(); }}>Pause</button>
+        : <button type="button" onClick={() => { setIsPlaying(prev => true); controlPlayer(); }}>Play</button>
+      }
     </div>
   );
 }
